b339a2058dae5330506657e20b23046cda10896a
Saad Batwa <86385454+Saad5400@users.noreply.github.com>
2023-12-02 01:58:59 +0300

    Delete Assets directory

--- Assets/Scripts/HardAI.cs
+++
@@ -1,7 +0,0 @@
-public class HardAI : EasyAI
-{
-    void Awake()
-    {
-        enabled = GameManager.gameMode == GameMode.HardAI;
-    }
-}
--- Assets/Scripts/MediumAI.cs
+++
@@ -1,7 +0,0 @@
-public class MediumAI : EasyAI
-{
-    void Awake()
-    {
-        enabled = GameManager.gameMode == GameMode.MediumAI;
-    }
-}
